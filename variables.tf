78d229e7baba8d4021ba5ed3aa7e61558c83461e
rizaldy <oss@rizaldy.club>
2022-08-06 05:03:40 +0700

    feat: add namecheap variables

--- variables.tf
+++ variables.tf
@@ -1,3 +1,19 @@
 variable "do_token" {
   description = "get it from https://cloud.digitalocean.com/account/api/tokens"
 }
+
+variable "dns_authoritaive_nameservers" {
+  default = ["ns1.digitalocean.com", "ns2.digitalocean.com", "ns3.digitalocean.com"]
+}
+
+variable "namecheap_username" {
+  type = string
+}
+
+variable "namecheap_api_user" {
+  type = string
+}
+
+variable "namecheap_api_key" {
+  description = "get it from https://ap.www.namecheap.com/settings/tools/apiaccess"
+}
