8569c1b19a97761a0c97eeb9db8144c1331b99d6
rizaldy <oss@rizaldy.club>
2022-08-06 05:02:39 +0700

    feat: add namecheap provider

--- main.tf
+++ main.tf
@@ -4,9 +4,19 @@ terraform {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
     }
+    namecheap = {
+      source  = "namecheap/namecheap"
+      version = ">= 2.0.0"
+    }
   }
 }
 
 provider "digitalocean" {
   token = var.do_token
+}
+
+provider "namecheap" {
+  user_name = var.namecheap_username
+  api_user  = var.namecheap_api_user
+  api_key   = var.namecheap_api_key
 }
