cd030ceace05cd36dbe34d54eabc3a0e20e71654
rizaldy <oss@rizaldy.club>
2022-12-26 16:37:20 +0700

    refactor: fix tyop

--- namecheap_dns.tf
+++ namecheap_dns.tf
@@ -2,12 +2,12 @@ resource "namecheap_domain_records" "evilfactorylabs-org" {
   domain = digitalocean_domain.evilfactorylabs.name
   mode   = "OVERWRITE"
 
-  nameservers = var.dns_authoritaive_nameservers
+  nameservers = var.dns_authoritative_nameservers
 }
 
 resource "namecheap_domain_records" "evlfctry-pro" {
   domain = digitalocean_domain.evlfctrypro.name
   mode   = "OVERWRITE"
 
-  nameservers = var.dns_authoritaive_nameservers
+  nameservers = var.dns_authoritative_nameservers
 }
