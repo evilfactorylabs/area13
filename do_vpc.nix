8449ae6311786e899b6d4ca4c44dd82cfd514ac8
rizaldy <oss@rizaldy.club>
2022-12-28 12:36:49 +0700

    feat: just testing

--- do_vpc.nix
+++ do_vpc.nix
@@ -4,4 +4,9 @@
     ip_range = "10.104.0.0/20";
     region = "sgp1";
   };
+  resource.digitalocean_vpc.dummy_sgp = {
+    name = "dummy-sgp";
+    ip_range = "10.105.69.0/20";
+    region = "sgp1";
+  };
 }
