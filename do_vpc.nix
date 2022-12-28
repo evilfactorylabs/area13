d159a648df318094d70b2bb53c2bd7b984a182de
rizaldy <oss@rizaldy.club>
2022-12-28 13:04:43 +0700

    fix: duplicates

--- do_vpc.nix
+++ do_vpc.nix
@@ -1,9 +1,4 @@
 {
-  resource.digitalocean_vpc.default_sgp = {
-    name = "default-sgp1";
-    ip_range = "10.104.0.0/20";
-    region = "sgp1";
-  };
   resource.digitalocean_vpc.dummy_sgp = {
     name = "dummy-sgp";
     ip_range = "10.105.69.0/20";
