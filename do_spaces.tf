0e12ffb4f9baa212f867457235bb6a6ee7591c20
rizaldy <oss@rizaldy.club>
2023-09-17 17:09:35 +0700

    fix: digitalocean spaces

--- do_spaces.tf
+++ do_spaces.tf
@@ -1,9 +1,6 @@
 resource "digitalocean_spaces_bucket" "evilfactorylabs-social" {
-  bucket_domain_name = "evilfactorylabs-social.sgp1.digitaloceanspaces.com"
-  id                 = "evilfactorylabs-social"
   name               = "evilfactorylabs-social"
   region             = "sgp1"
-  urn                = "do:space:evilfactorylabs-social"
 
   versioning {
     enabled = false
