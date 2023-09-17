85adaba84a36544586752216902722a986e0c65a
rizaldy <oss@rizaldy.club>
2023-09-17 16:47:18 +0700

    feat: move to digitalocean dns

--- namecheap_dns.tf
+++ namecheap_dns.tf
@@ -5,6 +5,13 @@ resource "namecheap_domain_records" "evilfactorylabs-org" {
   nameservers = var.dns_authoritative_nameservers
 }
 
+resource "namecheap_domain_records" "evilfactorylabs-social" {
+  domain = digitalocean_domain.evilfactorylabs_social.name
+  mode   = "OVERWRITE"
+
+  nameservers = var.dns_authoritative_nameservers
+}
+
 resource "namecheap_domain_records" "evlfctry-pro" {
   domain = digitalocean_domain.evlfctrypro.name
   mode   = "OVERWRITE"
